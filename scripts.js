53083048e04a2781955fff1e53d786e374148359
GobinathGS <52787966+GobinathGS@users.noreply.github.com>
2021-05-07 19:39:21 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,12 @@
+//select elements
+const selectElement = (s) => document.querySelector(s);
+
+//open menu
+selectElement('.open').addEventListener('click',() =>{
+    selectElement('.nav-list').classList.add('active');
+});
+
+//close menu
+selectElement('.close').addEventListener('click',() =>{
+    selectElement('.nav-list').classList.remove('active');
+});
